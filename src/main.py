2f6af736200f2268a64affc728101407bc533c53
Eugenio <iuke@Eugenios-iMac-Pro.local>
2020-09-13 18:53:47 -0300

    reorganized directories

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from perceptron import SimplePerceptron
 import os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+os.chdir('../')
 import json
 with open(os.getcwd() + "/input.json") as file:
     operand = json.load(file)['FUNCTION']
