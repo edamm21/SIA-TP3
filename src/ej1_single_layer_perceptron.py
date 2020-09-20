8874981d76b94f13ffbf9dfb41b1555a228caf07
EugeDamm <euge.damm@gmail.com>
2020-09-20 10:15:00 -0300

    added plotter class

--- src/ej1_single_layer_perceptron.py
+++ src/ej1_single_layer_perceptron.py
@@ -1,8 +1,8 @@
-from matplotlib import pyplot as plt 
 import numpy as np
 import random
 from datetime import datetime
 from file_reader import Reader
+from plotter import Plotter
 
 class SimplePerceptron:
     
@@ -11,42 +11,6 @@ class SimplePerceptron:
         self.iterations = iterations
         self.linear = linear
         self.beta = beta
-    
-    def create_plot(self, data, weights, operand):
-        fig,ax = plt.subplots()
-        ax.set_title(operand)
-        ax.set_xlabel("x")
-        ax.set_ylabel("y")
-
-        map_min = -1.5
-        map_max = 2
-        res = 0.5
-        x = np.linspace(-1.5, 2, 10) # te devuelve un intervalo entre -1.5 y 2 con numeros espaciados iguales para graficar el hiperplano
-        plt.plot(x, -((weights[0] + weights[1] * x) / weights[2]), '-g', label='Hiperplano')
-
-        positives = [[],[]]
-        negatives = [[],[]]
-        for i in range(len(data)):
-            x = data[i][1]
-            y = data[i][2]
-            desired  = data[i][-1]
-            if desired == 1:
-                positives[0].append(x)
-                positives[1].append(y)
-            else:
-                negatives[0].append(x)
-                negatives[1].append(y)
-
-        plt.xlim(map_min,map_max - 0.5)
-        plt.ylim(map_min,map_max - 0.5)
-
-        plt.scatter(negatives[0], negatives[1], s = 40.0, c = 'r', label = 'Proyeccion w < 0')
-        plt.scatter(positives[0], positives[1], s = 40.0, c = 'b', label = 'Proyeccion w > 0')
-
-        plt.legend(fontsize = 8, loc = 0)
-        plt.grid(True)
-        plt.show()
-        return
 
     def step(self, x): # funcion de activacion escalon
         if x > 0.0: return 1.0 
@@ -108,6 +72,7 @@ class SimplePerceptron:
                     w_min = weights
                 else:
                     break
-        self.create_plot(data, w_min, operand)
+        plotter = Plotter()
+        plotter.create_plot_ej1(data, weights, operand)
         return
 
